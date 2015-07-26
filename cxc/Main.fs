54f5dca9a60a0df4a2b9edc5c5d00c3e9f70d65d
Sandilya Jandhyala <jysandilya@gmail.com>
2015-07-26 21:38:27 +0530

    testing

--- cxc/Main.fs
+++ cxc/Main.fs
@@ -54,7 +54,7 @@ module ApplicationLogic =
     let waitingStatus = { NumberPlateStatus = Waiting; PersonDataStatus = Waiting; CoordsStatus = Waiting }
     let verifiedStatus = { NumberPlateStatus = Verified; PersonDataStatus = Verified; CoordsStatus = Verified }
     let deniedStatus = { NumberPlateStatus = Denied; PersonDataStatus = Denied; CoordsStatus = Denied }
-    let status = ref waitingStatus
+    let status = ref { NumberPlateStatus = Waiting; PersonDataStatus = Waiting; CoordsStatus = Waiting }
 
     let gateStatus = ref Closed
 
